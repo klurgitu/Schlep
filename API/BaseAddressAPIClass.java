5c377c0270c98ee1327310fa9073071273651dce
JJStadl2 <69908346+JJStadl2@users.noreply.github.com>
2020-09-25 09:11:54 -0400

    Changed feature components on pickup Address page
    Change from list to menu button for population of City and State API

--- API/BaseAddressAPIClass.java
+++
@@ -1,9 +0,0 @@
-package API;
-/**
- * This is a BaseAPI Class for our Country, State, City API
- * @author Katelynn Urgitus
- * Last Updated 09/24/2020
- */
-public class BaseAddressAPIClass {
-    protected final static CountryStateCityAPIInterface universalAPI = new UniversalTutorialAPITranslator();
-}
