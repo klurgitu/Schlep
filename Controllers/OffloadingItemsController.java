02ccdf1df41940a7e4a0bf7aeb7020470f6c44ba
JJStadl2 <69908346+JJStadl2@users.noreply.github.com>
2020-10-02 10:15:05 -0400

    Added assistance times to offloading items
    added time frames for assistance to offloading items page. added components like logo and back button. update color scheme per last meeting

--- Controllers/OffloadingItemsController.java
+++ Controllers/OffloadingItemsController.java
@@ -1,35 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Controllers;
 
-/**
- * This is a Controller for user entering their offloading items
- * @author Katelynn Urgitus
- * Last Updated 09/22/2020
- */
-import java.io.IOException;
 import java.net.URL;
-import Models.MoveScene;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
 
+/**
+ * FXML Controller class
+ *
+ * @author josia
+ */
 public class OffloadingItemsController implements Initializable {
 
+    @FXML
+    private Button nextBtn;
+    @FXML
+    private ImageView logo;
+    @FXML
+    private Label selectItemsLbl;
+    @FXML
+    private MenuButton menuBtn;
     @FXML
     private CheckMenuItem handTruckItem;
     @FXML
     private CheckMenuItem rampItem;
     @FXML
     private CheckMenuItem dollyItem;
-
-    private final MoveScene moveToBilling = new MoveScene();
     @FXML
-    private Button nextBtn;
+    private Label driverAssistLbl;
     @FXML
-    private MenuButton menuBtn;
+    private MenuButton timeOfAssistMnBtn;
+    @FXML
+    private MenuItem assistMnItem_30;
+    @FXML
+    private MenuItem assistMnItem_1Hr;
+    @FXML
+    private MenuItem assistTimeMnItem_2Hr;
+    @FXML
+    private MenuItem assistMnItem_3hr;
+    @FXML
+    private Button backBtn;
 
     /**
      * Initializes the controller class.
@@ -37,15 +59,14 @@ public class OffloadingItemsController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }
-/**
- * Will save the offloading items for the User into the Schlep Database
- * and move to the Billing screen
- * @param event
- * @throws IOException
- */
+    }    
+
     @FXML
-    private void saveItemsAndMove(ActionEvent event) throws IOException{
-       moveToBilling.Move("Billing.fxml", nextBtn);
+    private void saveItemsAndMove(ActionEvent event) {
     }
+
+    @FXML
+    private void setTimeOfAssist(ActionEvent event) {
+    }
+    
 }
