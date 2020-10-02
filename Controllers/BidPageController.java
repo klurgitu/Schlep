ef1d150f48d96c1137a4aec30815dd03d66ecde6
Marc <marc.bittle@gmail.com>
2020-10-02 17:16:06 -0400

    Updates to BidPage

--- Controllers/BidPageController.java
+++ Controllers/BidPageController.java
@@ -5,7 +5,6 @@
  */
 package Controllers;
 
-import com.gluonhq.charm.glisten.control.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -13,10 +12,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-<<<<<<< HEAD
-=======
 import javafx.scene.control.TextField;
->>>>>>> Updating branch
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
@@ -32,118 +28,64 @@ public class BidPageController implements Initializable {
     @FXML
     private Label driver1Lbl;
     @FXML
-<<<<<<< HEAD
     private TextField hourEst1TxFld;
     @FXML
-=======
->>>>>>> Updating branch
     private Button schlepItBtn1;
     @FXML
     private Label rating1Lbl;
     @FXML
-<<<<<<< HEAD
     private TextField price1TxtFld;
     @FXML
-=======
->>>>>>> Updating branch
     private Label BidLbl;
     @FXML
     private Label driver2Lbl;
     @FXML
-<<<<<<< HEAD
     private TextField hourEst2TxFld;
     @FXML
-=======
->>>>>>> Updating branch
     private Button schlepItBtn2;
     @FXML
     private Label rating2Lbl;
     @FXML
-<<<<<<< HEAD
     private TextField price2TxtFld;
     @FXML
     private Label driver3Lbl;
     @FXML
     private TextField hourEst3TxFld;
     @FXML
-=======
-    private Label driver3Lbl;
-    @FXML
->>>>>>> Updating branch
     private Button schlepItBtn3;
     @FXML
     private Label rating3Lbl;
     @FXML
-<<<<<<< HEAD
     private TextField price3TxtFld;
     @FXML
     private Label driver4Lbl;
     @FXML
     private TextField hourEst4TxFld;
     @FXML
-=======
-    private Label driver4Lbl;
-    @FXML
->>>>>>> Updating branch
     private Button schlepItBtn4;
     @FXML
     private Label rating4Lbl;
     @FXML
-<<<<<<< HEAD
     private TextField price4TxtFld;
     @FXML
     private Label driver5Lbl;
     @FXML
     private TextField hourEst5TxFld;
     @FXML
-=======
-    private Label driver5Lbl;
-    @FXML
->>>>>>> Updating branch
     private Button schlepItBtn5;
     @FXML
     private Label rating5Lbl;
     @FXML
-<<<<<<< HEAD
     private TextField price5TxtFld;
     @FXML
     private HBox driver6Lbl;
     @FXML
     private TextField hourEst6TxFld;
     @FXML
-=======
-    private HBox driver6Lbl;
-    @FXML
->>>>>>> Updating branch
     private Button schlepItBtn6;
     @FXML
     private Label rating6Lbl;
     @FXML
-<<<<<<< HEAD
-=======
-    private TextField hourEst1TxFld;
-    @FXML
-    private TextField price1TxtFld;
-    @FXML
-    private TextField hourEst2TxFld;
-    @FXML
-    private TextField price2TxtFld;
-    @FXML
-    private TextField hourEst3TxFld;
-    @FXML
-    private TextField price3TxtFld;
-    @FXML
-    private TextField hourEst4TxFld;
-    @FXML
-    private TextField price4TxtFld;
-    @FXML
-    private TextField hourEst5TxFld;
-    @FXML
-    private TextField price5TxtFld;
-    @FXML
-    private TextField hourEst6TxFld;
-    @FXML
->>>>>>> Updating branch
     private TextField price6TxtFld;
 
     /**
@@ -152,7 +94,6 @@ public class BidPageController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-<<<<<<< HEAD
     }
 
     @FXML
@@ -160,13 +101,4 @@ public class BidPageController implements Initializable {
 
     }
 
-=======
-    }    
-
-    @FXML
-    private void acceptBid(ActionEvent event) {
-        
-    }
-    
->>>>>>> Updating branch
 }
