818ca27a85a1db750539d74e94066696617f536b
JJStadl2 <69908346+JJStadl2@users.noreply.github.com>
2020-10-16 11:02:09 -0400

    Update RunAdditionalItems.java
    Copied original code and changed FXML being read by loader to check connections for other pages

--- Models/RunAdditionalItems.java
+++ Models/RunAdditionalItems.java
@@ -25,16 +25,34 @@ public class RunAdditionalItems extends Application{
     Scene scene;
     @Override
     public void start(Stage stage) throws Exception {
-        window = stage;
+        /*
+        *window = stage;
         window.setTitle("Additional Needs");
         URL file = new File("src/View/AdditionalNeedsPer.fxml").toURI().toURL();
         Parent root = FXMLLoader.load(file);
         scene = new Scene(root);
         window.setScene(scene);
         window.show();
+        */
+        
+         /*
+     * 
+     */
+        window = stage;
+        window.setTitle("");
+        URL file = new File("src/View/SelectDeliveryType.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(file);
+        scene = new Scene(root);
+        window.setScene(scene);
+        window.show();
+        
+        
+        
+        
     }
      public static void main(String args[]){
         launch(args);
     }
-    
+   
+     
 }
