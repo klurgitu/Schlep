6ad070c4ad9c249ea5e25c5844c3097ef8237b55
JJStadl2 <69908346+JJStadl2@users.noreply.github.com>
2020-10-16 12:07:46 -0400

    Update SelectDeliveryTypeController.java
    added parameter tags

--- Controllers/SelectDeliveryTypeController.java
+++ Controllers/SelectDeliveryTypeController.java
@@ -50,6 +50,8 @@ public class SelectDeliveryTypeController implements Initializable {
 
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
